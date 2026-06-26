a7f3861c6c9a19ef3edafcf0de28108625bc6ff1
iorgad-lab <i.orgad@alustudent.com>
2026-06-26 10:51:16 -0700

    fix task 8 final

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 word = "object-oriented programming with Python"
-print(word[:6] + word[6:19] + word[19:])
+print(word[0:6] + word[6:7] + word[7:19] + word[19:26] + word[26:])
 
 
