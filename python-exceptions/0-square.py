6667dadba5065056de3ab22c35836fb63545aeb7
iorgad-lab <i.orgad@alustudent.com>
2026-07-14 14:05:47 -0700

    Add python classes tasks

---
+++ python-exceptions/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for Square"""
+
+
+class Square:
+    """Represent a square"""
+    pass
---
+++ python-exceptions/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for Square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialize a new Square"""
+        self.__size = size
