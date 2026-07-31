7f7c0e640ce734a40b73ce38e600679cb792b413
orgad <i.orgad@alustudent.com>
2026-07-31 22:34:06 +0200

    Guard 0 and 4 with __name__ == __main__ to prevent import-time network call

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,7 +2,8 @@
 """Script that fetches https://alu-intranet.hbtn.io/status using requests."""
 import requests
 
-response = requests.get("https://alu-intranet.hbtn.io/status")
-print("Body response:")
-print("\t- type: {}".format(type(response.text)))
-print("\t- content: {}".format(response.text))
+if __name__ == "__main__":
+    response = requests.get("https://alu-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
