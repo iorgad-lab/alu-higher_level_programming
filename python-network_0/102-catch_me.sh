6600a8b2c64effccf090273df1c919d5c1839d63
orgad <i.orgad@alustudent.com>
2026-07-31 22:11:56 +0200

    Fix 102-catch_me.sh to follow the real 3-step redirect chain

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to /catch_me and lets curl print the body directly
-curl -s "0.0.0.0:5000/catch_me"
+# Makes a chained PUT request to /catch_me and prints the final response
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "0.0.0.0:5000/catch_me"
