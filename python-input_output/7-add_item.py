8cf4910fc62ba9759a454f54df44ead890596c12
orgad <i.orgad@alustudent.com>
2026-07-31 13:48:37 +0200

    Make 7-add_item.py self-contained

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Script that adds command line arguments to a JSON list file."""
+import json
 import os
 import sys
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
 if os.path.exists(filename) and os.path.getsize(filename) > 0:
-    items = load_from_json_file(filename)
+    with open(filename, encoding="utf-8") as f:
+        items = json.load(f)
 else:
     items = []
 
 items.extend(sys.argv[1:])
-save_to_json_file(items, filename)
+
+with open(filename, "w", encoding="utf-8") as f:
+    json.dump(items, f)
