7ed1de39d8c2c62f8f1eaeed892ddd4e75365c58
orgad <i.orgad@alustudent.com>
2026-07-31 13:39:40 +0200

    Fix 7-add_item.py crash on empty existing file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,7 +8,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-if os.path.exists(filename):
+if os.path.exists(filename) and os.path.getsize(filename) > 0:
     items = load_from_json_file(filename)
 else:
     items = []
