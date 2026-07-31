69be70fa3d74be8ecafc9ae038f7257b153c653d
orgad <i.orgad@alustudent.com>
2026-07-31 21:57:23 +0200

    Follow redirects with -L in 1-body.sh and 4-header.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the body of the response only if the status code is 200
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -s -L -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s -L "$1"
