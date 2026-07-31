39fc5e29f54f2ef562a783b0fa5437a17d365b63
orgad <i.orgad@alustudent.com>
2026-07-31 22:39:47 +0200

    Add task 100 github_commits to python-network_1

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that lists the 10 most recent commits of a GitHub repository."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+    response = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit.get("sha")
+        name = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, name))
