1c50c478575cd8a3c5723adb42ee70ec7a5f4d7e
orgad <i.orgad@alustudent.com>
2026-08-02 15:22:42 +0200

    Reuse matrix_mul's custom validation in lazy_matrix_mul

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,4 +7,29 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices m_a and m_b using NumPy."""
+    def validate(m, name):
+        if not isinstance(m, list):
+            raise TypeError("{} must be a list".format(name))
+        if not all(isinstance(row, list) for row in m):
+            raise TypeError("{} must be a list of lists".format(name))
+        if m == [] or m == [[]]:
+            raise ValueError("{} can't be empty".format(name))
+        for row in m:
+            for item in row:
+                if not isinstance(item, (int, float)) or \
+                        isinstance(item, bool):
+                    raise TypeError(
+                        "{} should contain only integers or floats"
+                        .format(name))
+        row_len = len(m[0])
+        if not all(len(row) == row_len for row in m):
+            raise TypeError(
+                "each row of {} must be of the same size".format(name))
+
+    validate(m_a, "m_a")
+    validate(m_b, "m_b")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
     return np.matmul(m_a, m_b)
