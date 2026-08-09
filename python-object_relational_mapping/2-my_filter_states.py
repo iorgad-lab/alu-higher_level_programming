17a5a7b85ea762dfcb64f1021c2f8f02ee1bf969
orgad <i.orgad@alustudent.com>
2026-08-09 17:45:50 +0200

    Add task 2 my_filter_states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Lists all states matching user input (NOT safe from SQL injection)"""
+"""Script that lists states matching a user-provided name."""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                          user=sys.argv[1], passwd=sys.argv[2],
-                          db=sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
     cur.execute(query.format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
     cur.close()
     db.close()
