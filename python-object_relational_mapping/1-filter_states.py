26734f9d5d0f5a1e0fe78c92b3454d1ec60cfc6f
orgad <i.orgad@alustudent.com>
2026-08-09 17:42:17 +0200

    Add task 1 filter_states with case-sensitive matching

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from hbtn_0e_0_usa"""
+"""Script that lists states starting with N from hbtn_0e_0_usa."""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                          user=sys.argv[1], passwd=sys.argv[2],
-                          db=sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' "
-                "ORDER BY states.id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+        "ORDER BY states.id ASC")
+    for row in cur.fetchall():
         print(row)
     cur.close()
     db.close()
