67c7ffbc0c16a188328e589407c094b54b286d2f
orgad <i.orgad@alustudent.com>
2026-08-09 17:48:59 +0200

    Fix case-sensitive exact match in my_filter_states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
         host="localhost", port=3306,
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+    query = ("SELECT * FROM states WHERE BINARY name = '{}' "
+             "ORDER BY states.id ASC")
     cur.execute(query.format(sys.argv[4]))
     for row in cur.fetchall():
         print(row)
